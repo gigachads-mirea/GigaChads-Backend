2c44d1351e9317aba5533b65d79fd412ec10ca89
vadniks <whatever@fake.some>
2023-05-18 23:12:49 +0300

    Backend-3: Added temporary solution to resolve 'UserMapper bean could not be found' issue

--- src/main/java/ru/gigachads/GigachadsApplication.java
+++ src/main/java/ru/gigachads/GigachadsApplication.java
@@ -2,11 +2,13 @@ package ru.gigachads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Описание класса
  */
 @SpringBootApplication
+@ComponentScan("ru.gigachads.mapper") // TODO: temporary solution to resolve the 'bean UserMapper could not be found' issue
 public class GigachadsApplication {
 
     public static void main(String[] args) {
