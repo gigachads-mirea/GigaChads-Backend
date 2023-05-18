25ea141155f23d71fe80d3b1252c12ae9f3b4dae
vadniks <whatever@fake.some>
2023-05-18 23:15:34 +0300

    Backend-3: Cleaned up in the ServerRepository

--- src/main/java/ru/gigachads/repository/ServerRepository.java
+++ src/main/java/ru/gigachads/repository/ServerRepository.java
@@ -1,31 +1,9 @@
 package ru.gigachads.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.lang.Nullable;
 import ru.gigachads.entity.Server;
 
 /**
  * Описание класса
  */
-public interface ServerRepository extends JpaRepository<Server, Long> {
-
-    @Nullable
-    @Query(value = "select * from servers where ipAddress = ?1", nativeQuery = true)
-    Server getByIpAddress(String ipAddress); // ip address is unique for each server?
-
-//    @Query(value = "select * from servers where capacity = ?1", nativeQuery = true)
-//    List<Server> getByCapacity(int capacity); // TODO: remove these
-//
-//    @Query(value = "select * from servers where region = ?1", nativeQuery = true)
-//    List<Server> getByRegion(String region);
-//
-//    @Query(value = "select * from servers where antiCheat = ?1", nativeQuery = true)
-//    List<Server> getByAntiCheat(boolean antiCheat);
-//
-//    @Query(value = "select * from servers where rating = ?1", nativeQuery = true)
-//    List<Server> getByRating(float rating);
-//
-//    @Query(value = "select * from servers where gameId = ?1", nativeQuery = true)
-//    List<Server> getByGameId(float gameId);
-}
+public interface ServerRepository extends JpaRepository<Server, Long> {}
